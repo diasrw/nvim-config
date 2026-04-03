3e362b684a33c31c0205ca8c8a636410ee66da3d
EnDpim <rivith.wijesekara@gmail.com>
2026-04-03 23:36:32 +0530

    change and stuffs

--- lua/phim/remap.lua
+++ lua/phim/remap.lua
@@ -14,7 +14,7 @@ vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
